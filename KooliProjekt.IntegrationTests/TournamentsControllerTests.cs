17c9383d4bca88de96b3dab993013919e694624f
Danel Filippov <Danel.Filippov@tptlive.ee>
2026-03-16 19:20:21 +0200

    0219

---
+++ KooliProjekt.IntegrationTests/TournamentsControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using KooliProjekt.Application.Data;
+using KooliProjekt.Application.Infrastructure.Paging;
+using KooliProjekt.Application.Infrastructure.Results;
+using KooliProjekt.IntegrationTests.Helpers;
+using Xunit;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class TournamentsControllerTests : TestBase
+    {
+        [Fact]
+        public async Task List_should_return_paged_result()
+        {
+            // Arrange
+            var url = "/api/Tournaments/List/?page=0&pageSize=0";
+
+            // Act
+            var response = await Client.GetFromJsonAsync<OperationResult<PagedResult<Tournament>>>(url);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.False(response.HasErrors);
+        }
+
+        [Fact]
+        public async Task Get_should_return_list()
+        {
+            // Arrange
+            var url = "/api/Tournaments/Get/?id=1";
+            
+            var tournament = new Tournament { Title = "Test List" };
+            await DbContext.AddAsync(tournament);
+            await DbContext.SaveChangesAsync();
+
+            // Act
+            var response = await Client.GetFromJsonAsync<OperationResult<Tournament>>(url);
+            
+            // Assert
+            Assert.NotNull(response);
+            Assert.False(response.HasErrors);
+        }
+
+        [Fact]
+        public async Task Get_should_return_not_found_for_missing_list()
+        {
+            // Arrange
+            var url = "/api/Tournaments/Get/?id=131";
+
+            // Act
+            var response = await Client.GetAsync(url);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ KooliProjekt.IntegrationTests/UsersControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using KooliProjekt.Application.Data;
+using KooliProjekt.Application.Infrastructure.Paging;
+using KooliProjekt.Application.Infrastructure.Results;
+using KooliProjekt.IntegrationTests.Helpers;
+using Xunit;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class UsersControllerTests : TestBase
+    {
+        [Fact]
+        public async Task List_should_return_paged_result()
+        {
+            // Arrange
+            var url = "/api/Users/List/?page=0&pageSize=0";
+
+            // Act
+            var response = await Client.GetFromJsonAsync<OperationResult<PagedResult<User>>>(url);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.False(response.HasErrors);
+        }
+
+        [Fact]
+        public async Task Get_should_return_list()
+        {
+            // Arrange
+            var url = "/api/Users/Get/?id=1";
+            
+            var user = new User { Title = "Test List" };
+            await DbContext.AddAsync(user);
+            await DbContext.SaveChangesAsync();
+
+            // Act
+            var response = await Client.GetFromJsonAsync<OperationResult<User>>(url);
+            
+            // Assert
+            Assert.NotNull(response);
+            Assert.False(response.HasErrors);
+        }
+
+        [Fact]
+        public async Task Get_should_return_not_found_for_missing_list()
+        {
+            // Arrange
+            var url = "/api/Users/Get/?id=131";
+
+            // Act
+            var response = await Client.GetAsync(url);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
