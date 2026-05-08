28ce14a5d10f35d654a7c0d0f9597201fa75b4e3
Danel Filippov <Danel.Filippov@tptlive.ee>
2026-05-09 01:00:41 +0300

    0904

---
+++ KooliProjekt.WpfApplication/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KooliProjekt.WpfApplication
+{
+    public class MainWindowViewModel
+    {
+        public IList<User> Data 
+        { 
+            get
+            {
+                var items = new List<User>
+                {
+                    new User { Id = 1, Title = "Test 1" },
+                    new User { Id = 2, Title = "Test 2" },
+                    new User{ Id = 3, Title = "Test 3" }
+                };
+
+                return items;
+            }
+        }
+
+        public object SelectedItem { get; set; }
+    }
+}
